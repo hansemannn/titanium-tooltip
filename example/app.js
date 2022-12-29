275d7b6492fbf1e5146908743ca0f7dcc82fdc9a
Mitul Bhalia <mitul.bhaliya@gmail.com>
2022-12-29 19:41:17 +0530

    added some missing properties

--- example/app.js
+++ example/app.js
@@ -18,13 +18,19 @@ btn.addEventListener('click', () => {
         title: 'This is a dummy text line 1.This is a dummy text line 2.',
         backgroundColor: '#61c494',
         borderRadius: 40,
+        borderColor: '#ff0000',
+        borderWidth: 2,
+        padding: 10,
         arrowWidth: 20,
         arrowHeight: 24,
         arrowMargin: 10,
         textColor: '#fff',
-        container: win,
+        container: window,
         sourceView: btn,
-        direction: Tooltips.TOOLTIP_DIRECTION_UP
+        direction: Tooltips.TOOLTIP_DIRECTION_UP,
+        onClick: () => {
+        alert('Tooltip tapped!');
+    }
     })
 });
 
